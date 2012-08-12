31ddda8988561506d13687da7c754738bc5c8900
Kalinin Eugene <e.v.kalinin@gmail.com>
2012-08-13 01:19:01 +0400

    used new theme for html

--- src/conf.py
+++ src/conf.py
@@ -42,8 +42,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'learnyousomeerlang.ru'
-copyright = u'2012, Eugene Kalinin'
+project = u'LearnYouSomeErlang.ru'
+copyright = u'2012, Калинин Евгений'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -96,7 +96,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-html_theme = 'haiku'
+#html_theme = 'basic'
+html_theme = 'lyse'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -104,11 +105,11 @@ html_theme = 'haiku'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['../themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "learn you some Erlang"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
